8c674acfec4d9b3a6b8de83a63f18ab922ff9558
Ossian Eriksson <OssianEriksson@users.noreply.github.com>
2022-04-16 18:03:24 +0200

    Link to GitHub

--- src/routes/_Footer.svelte
+++ src/routes/_Footer.svelte
@@ -1,3 +1,8 @@
 <div class="bg-gray-200 flex flex-col items-center">
-  <span class="m-4">{new Date().getUTCFullYear()}</span>
+  <span class="m-4">
+    <a class="hover:underline" href="https://github.com/OssianEriksson/255.se/tree/main/src/routes">
+      Redigera på GitHub
+    </a>
+    | {new Date().getUTCFullYear()}
+  </span>
 </div>
