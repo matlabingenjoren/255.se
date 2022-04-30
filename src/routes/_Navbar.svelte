ea7c98dc2f19138d1fc1fe2bcb282bfe64479cde
Ossian Eriksson <OssianEriksson@users.noreply.github.com>
2022-04-30 15:58:42 +0200

    Fix wrong media query

--- src/routes/_Navbar.svelte
+++ src/routes/_Navbar.svelte
@@ -108,7 +108,7 @@
                 <div
                   class={`h-1 bg-gray-300 lg:bg-gray-400 ${
                     selected.includes(i) ? 'w-full' : 'w-0'
-                  } md:group-hover:w-full transition-[width] m-auto`}
+                  } lg:group-hover:w-full transition-[width] m-auto`}
                 />
               </div>
             </div>
