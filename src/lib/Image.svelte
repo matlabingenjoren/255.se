8da21c9a80dabd7686f940549336ebbfbc50ada9
Ossian Eriksson <OssianEriksson@users.noreply.github.com>
2022-04-30 15:52:50 +0200

    Fix image loading in firefox

--- src/lib/Image.svelte
+++ src/lib/Image.svelte
@@ -10,21 +10,28 @@
   let clazz: string | undefined = undefined
   export { clazz as class }
 
-  let loadad = false
+  let img: HTMLImageElement
+  let loaded = false
+  $: loaded ||= img && img.complete && img.naturalWidth > 0
 </script>
 
 <div class={`relative ${clazz || ''}`}>
   <div
     class={`overflow-hidden z-10 relative w-full h-full bg-gray-400 transition-opacity ${
-      loadad ? 'opacity-0' : 'opacity-100'
+      loaded ? 'opacity-0' : 'opacity-100'
     }`}
   >
     <img class="w-full h-full blur-lg" src={src.placeholder} alt={alt || src.alt} />
   </div>
-  <picture class="absolute top-0 left-0 w-full h-full">
+  <picture
+    class="absolute top-0 left-0 w-full h-full"
+    on:load={() => {
+      console.log('Picture loaded')
+    }}
+  >
     {#each src.sources as imgSrc}
       <source {...imgSrc} />
     {/each}
-    <img src={src.placeholder} alt={alt || src.alt} loading="lazy" on:load={() => (loadad = true)} />
+    <img bind:this={img} src={src.placeholder} alt={alt || src.alt} loading="lazy" on:load={() => (loaded = true)} />
   </picture>
 </div>
